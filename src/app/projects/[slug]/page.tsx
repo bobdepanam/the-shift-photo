26806ee4dcecaf960aa1df6fce3fdccf54559e46
bobdepanam <48792502+bobdepanam@users.noreply.github.com>
2025-07-10 19:04:24 +0200

    🚑 fix: remove type alias for Props to resolve Vercel build issue

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,23 +1,18 @@
-// app/projects/[slug]/page.tsx
+// src/app/projects/[slug]/page.tsx
 
 import { notFound } from 'next/navigation'
 import { getProjectBySlug, getAllProjectSlugs } from '@/lib/project'
 import ProjectPageClient from './ProjectPageClient'
 import { Project } from '@/types/project'
 
-type Props = {
-  params: {
-    slug: string
-  }
-}
-
 export async function generateStaticParams() {
   const slugs = await getAllProjectSlugs()
   return slugs.map(({ slug }) => ({ slug }))
 }
 
-export default async function Page({ params }: Props) {
+export default async function Page({ params }: { params: { slug: string } }) {
   const slug = params.slug
+
   if (!slug) return notFound()
 
   const project: Project | null = await getProjectBySlug(slug)
